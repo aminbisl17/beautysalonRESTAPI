3fd214f916baf55bcc9b11a4f991c85d459dcc5c
Amin <amin.b05@icloud.com>
2025-12-06 19:12:51 +0100

    v0.11.0

--- src/main/java/com/example/beautysalonRESTAPI/backend/Configuration/CorsConfig.java
+++ src/main/java/com/example/beautysalonRESTAPI/backend/Configuration/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.beautysalonRESTAPI.backend.security;
+package com.example.beautysalonRESTAPI.backend.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
