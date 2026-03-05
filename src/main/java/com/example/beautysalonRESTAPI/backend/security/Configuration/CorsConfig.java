e379baaf85b5b99281ee146813164fb5a8d5ae74
Amin <amin.b05@icloud.com>
2026-03-05 21:07:55 +0100

    v2.2.5

--- src/main/java/com/example/beautysalonRESTAPI/backend/security/Configuration/CorsConfig.java
+++ src/main/java/com/example/beautysalonRESTAPI/backend/security/Configuration/CorsConfig.java
@@ -7,23 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
                     .allowedOriginPatterns(
-                        "http://localhost:3000", 
-                        "https://192.168.*.*:3000",
-                        "http://192.168.*.*:3000"
+                        "http://localhost:3000",
+                        "http://192.168.*:3000"
                     )
                     .allowedMethods("*")
                     .allowedHeaders("*")
-                    .exposedHeaders("*")
                     .allowCredentials(true);
-            }
-        };
-    }
+        }
+    };
+}
 }
