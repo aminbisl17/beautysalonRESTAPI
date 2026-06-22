0e1296788b69979c1f3c4e41a905b21782513a6c
Amin <amin.b05@icloud.com>
2026-06-22 20:23:37 +0200

    v3.10.0

--- src/main/java/com/example/beautysalonRESTAPI/api/ClientSide/WebClient.java
+++ src/main/java/com/example/beautysalonRESTAPI/api/ClientSide/WebClient.java
@@ -14,6 +14,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -37,6 +40,13 @@ public ResponseEntity<?> getMethodName() {
         return ResponseEntity.noContent().build();
     }
     return ResponseEntity.ok().body(employees);
+}
+
+@PostMapping("fastAuthenticate")
+public String postMethodName(@RequestBody String entity) {
+    //TODO: process POST request
+    
+    return entity;
 }
 
 }
