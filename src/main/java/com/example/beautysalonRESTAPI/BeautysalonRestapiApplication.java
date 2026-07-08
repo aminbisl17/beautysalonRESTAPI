a09963a5d1127293065286a9b9a365ac0b5a5472
Amin <amin.b05@icloud.com>
2026-07-08 22:58:07 +0200

    v3.12.5

--- src/main/java/com/example/beautysalonRESTAPI/BeautysalonRestapiApplication.java
+++ src/main/java/com/example/beautysalonRESTAPI/BeautysalonRestapiApplication.java
@@ -12,8 +12,6 @@ public class BeautysalonRestapiApplication {
 	public static void main(String[] args) {
 
        Dotenv dotenv = Dotenv.load();
-	
-
          System.setProperty("MAIL_HOST", dotenv.get("MAIL_HOST"));
         System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
         System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
