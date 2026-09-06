3bc671770f3181b58e0d6076d1303f28fef63396
Amin <amin.b05@icloud.com>
2026-09-06 18:51:54 +0200

    v4.10.6

--- src/main/java/com/example/beautysalonRESTAPI/Configuration/DataSourceConfig.java
+++ src/main/java/com/example/beautysalonRESTAPI/Configuration/DataSourceConfig.java
@@ -11,6 +11,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
+    /* 
     @Bean
     public DataSource dataSource() {
         Dotenv dotenv = Dotenv.load();
@@ -20,5 +21,17 @@ public class DataSourceConfig {
         config.setPassword(dotenv.get("DB_PASSWORD"));
         config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         return new HikariDataSource(config);
-    }
+    } */
+
+        @Bean
+public DataSource dataSource() {
+    HikariConfig config = new HikariConfig();
+
+    config.setJdbcUrl(System.getenv("DB_URL"));
+    config.setUsername(System.getenv("DB_USERNAME"));
+    config.setPassword(System.getenv("DB_PASSWORD"));
+    config.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+    return new HikariDataSource(config);
+}
 }
