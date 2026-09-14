76da95776785276e7fa07e167688f7f0801742af
Amin <amin.b05@icloud.com>
2026-09-14 19:48:54 +0200

    v4.10.8

--- src/main/java/com/example/beautysalonRESTAPI/Configuration/CorsConfig.java
+++ src/main/java/com/example/beautysalonRESTAPI/Configuration/CorsConfig.java
@@ -4,28 +4,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.Arrays;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                       .allowedOriginPatterns(
-                                "http://localhost:3000",
-                                "http://localhost:3001",
-                                "http://192.168.*:3000",
-                                "http://192.168.*:3001",
-                                "https://beautysalonclientreact.onrender.com",
-                             "https://beautysalonadminreact.onrender.com"
-                        )
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://192.168.*:3000",
+                "http://192.168.*:3001",
+                "https://beautysalonclientreact.onrender.com",
+                "https://beautysalonadminreact.onrender.com"
+        ));
+
+        configuration.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "PATCH",
+                "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
